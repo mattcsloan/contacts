3b8bdbccf8e08fa8f1d724b0a3ee1662b5af0882
Matt Sloan <sloan@Matts-MacBook-Pro.local>
2017-07-04 23:56:05 -0400

    Added search input field to eventually filter contacts

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -7,27 +7,46 @@ class ListContacts extends Component {
     onDeleteContact: PropTypes.func.isRequired
   };
 
+  state = {
+    query: ''
+  };
+
+  updateQuery = (query) => {
+    this.setState({ query: query.trim() })
+  };
+
   render() {
     return (
-      <ol className="contact-list">
-        {
-          this.props.contacts.map(contact => (
-            <li className="contact-list-item" key={contact.id}>
-              <div className="contact-avatar" style={{
-                backgroundImage: `url(${contact.avatarURL})`
-              }} />
-              <div className="contact-details">
-                <p>{contact.name}</p>
-                <p>{contact.email}</p>
-              </div>
-              <button
-                className="contact-remove"
-                onClick={() => this.props.onDeleteContact(contact)}
-              >Remove</button>
-            </li>
-          ))
-        }
-      </ol>
+      <div className="list-contacts">
+        <div className="list-contacts-top">
+          <input
+            className="search-contacts"
+            type="text"
+            placeholder="Search contacts"
+            value={this.state.query}
+            onChange={(event) => this.updateQuery(event.target.value)}
+          />
+        </div>
+        <ol className="contact-list">
+          {
+            this.props.contacts.map(contact => (
+              <li className="contact-list-item" key={contact.id}>
+                <div className="contact-avatar" style={{
+                  backgroundImage: `url(${contact.avatarURL})`
+                }} />
+                <div className="contact-details">
+                  <p>{contact.name}</p>
+                  <p>{contact.email}</p>
+                </div>
+                <button
+                  className="contact-remove"
+                  onClick={() => this.props.onDeleteContact(contact)}
+                >Remove</button>
+              </li>
+            ))
+          }
+        </ol>
+      </div>
     )
   }
 }
