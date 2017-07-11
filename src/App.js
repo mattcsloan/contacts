50ac35b2d1ff644da7040049e72f32efcedac9ca
Matt Sloan <sloan@Matts-MacBook-Pro.local>
2017-07-11 16:05:19 -0400

    updated the Create Contact form component to actually submit contact to server and add contact to state

--- src/App.js
+++ src/App.js
@@ -27,6 +27,14 @@ class App extends Component {
     ContactsAPI.remove(contact);
   }
 
+  createContact = (contact) => {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([ contact ])
+      }));
+    })
+  }
+
   render() {
     return (
       <div className="app">
@@ -37,7 +45,15 @@ class App extends Component {
           />
         )}
         />
-        <Route path="/create" component={CreateContact} />
+        <Route path="/create" render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact);
+              history.push('/');
+            }}
+          />
+        )}
+        />
       </div>
     );
   }
